18d93c26df89aa1ba135d09a894c273af6db4761
Scott Zhang <qinglian.zhang@dewmobile.net>
2016-01-28 15:28:54 +0800

    change bus to buz

--- app/src/main/java/com/zql/android/bus_test/MainActivity.java
+++ app/src/main/java/com/zql/android/bus_test/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.util.TimeUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
